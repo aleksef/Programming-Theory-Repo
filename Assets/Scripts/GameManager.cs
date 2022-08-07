d33aefc41905e7154f060e43f35a7b96ecd6b25f
aleksef <38755134+aleksef@users.noreply.github.com>
2022-08-07 23:58:13 +0300

    add food check

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private BaseCreature[] creatures;
-    [SerializeField] GameObject wine;
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        creatures = FindObjectsOfType<BaseCreature>();
-        foreach (BaseCreature creature in creatures) 
-        {
-            creature.Idle();
-        }
-        */
-        BaseCreature human = FindObjectOfType<Human>();
-        human.Target = wine;
-        human.Walk();
+        CheckForFood();
+        StartCoroutine(FoodSearchCycle());
     }
 
-    private void Update()
+    public void CheckForFood()
     {
+        foreach (BaseCreature creature in FindObjectsOfType<BaseCreature>()) 
+        {
+            foreach (BaseFood food in FindObjectsOfType<BaseFood>())
+            {
+                if (creature.Edibles.Contains(food.tag)) 
+                {
+                    creature.Target = food.gameObject;
+                    creature.Run();
+                }
+            }
+        }
+    }
 
+    private IEnumerator FoodSearchCycle()
+    {
+        yield return new WaitForSeconds(3);
+        CheckForFood();
+        StartCoroutine(FoodSearchCycle());
     }
 }
