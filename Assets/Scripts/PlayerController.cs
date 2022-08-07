16bb7cf3d5f7981fda877d02146348b3a1bf3931
aleksef <38755134+aleksef@users.noreply.github.com>
2022-08-07 21:19:08 +0300

    add object selection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject pointer;
     [SerializeField] GameObject mainCamera;
     private Rigidbody mainCameraRb;
+
+    [SerializeField] GameObject[] spawnableObjects;
+    private GameObject selectedObject;
 
     private void Start()
     {
@@ -16,6 +20,15 @@ public class PlayerController : MonoBehaviour
     private void LateUpdate()
     {
         MoveCamera();
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            SpawnObject();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            selectedObject = null;
+        }
     }
 
     void MoveCamera() 
@@ -24,5 +37,19 @@ public class PlayerController : MonoBehaviour
             Input.GetAxis("Horizontal"),
             mainCameraRb.velocity.y,
             Input.GetAxis("Vertical")).normalized * 10;
+    }
+
+    void SpawnObject() 
+    {
+        if (selectedObject != null) 
+        {
+            Debug.Log("Spawn!");
+            Instantiate(selectedObject, pointer.transform.position, selectedObject.transform.rotation);
+        }
+    }
+
+    public void SelecObjectToSpawn(int value) 
+    {
+        selectedObject = spawnableObjects[value];
     }
 }
